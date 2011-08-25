af7877a18ff062ef5bbe716bf75aab034163dad7
Antonio Feitosa <antonio.cfc@gmail.com>
2011-08-25 13:57:36 -0300

    Exercicio1-19

--- exercicio1-19.c
+++ exercicio1-19.c
@@ -1,36 +1,44 @@
 #include <stdio.h>
 #define MAXLINHA	82 /* tamanho maximo da linha entrada */
 int lelinha(char [],int);
+int inverte(char [],int);
 int
 main(void)
 {
 	int tam; /* tamanho corrente da linha */
-	int tmp;
 	char linha[MAXLINHA];
-	tmp=0;
-	while((tam=lelinha(linha,MAXLINHA))>0)
-		if(!(tam<2 && linha[0]))
-			printf("%s",linha);
+	while((tam=lelinha(linha,MAXLINHA))>0) {
+		inverte(linha,tam);
+		printf("%s",linha);
+	}
 	return 0;
 }
 
-int lelinha(char s[], int lim)
+int
+lelinha(char s[], int lim)
 {
 	int c,i;
-	int n=0;
 	for(i=0;i<lim-1 && (c=getchar()) != EOF && c != '\n'; ++i) {
 		s[i]=c;
-		if(c==' ' || c=='\t')
-			n++;
-		else
-			n=0;
 	}
 	if(c=='\n') {
-		i=i-n;
 		s[i]=c;
 		++i;
 	}
 	s[i]='\0';
+	return i;
+}
+
+int
+inverte(char s[], int lim)
+{
+	int c,i;
+	int tam=lim-2;
+	for(i=0;i<tam/2; ++i) {
+		c=s[i];
+		s[i]=s[tam-i];
+		s[tam-i]=c;
+	}
 	return i;
 }
 
